30371723c520d8320b1e92dad1d965ba9f38bd8a
Divyansh Mishra <divyanshmishra24i2006@gmail.com>
2026-07-19 23:12:54 +0530

    Time: 2 ms (99.72%) | Memory: 45.3 MB (10.42%) - LeetSync

---
+++ 90-subsets-ii/subsets-ii.java
@@ -0,0 +1,28 @@
+class Solution {
+    static void solve (int[] nums, int i,List<Integer> output,List<List<Integer>> ans){
+        if(i>=nums.length){
+            ans.add(new ArrayList<>(output));
+            return ;
+        }
+        int val=nums[i];
+        output.add(val);
+        solve(nums,i+1,output,ans);
+        output.remove(output.size()-1);
+        while(i+1<nums.length && nums[i]==nums[i+1]){
+            i++;
+        }
+        solve(nums,i+1,output,ans);
+        
+    }
+
+    public List<List<Integer>> subsetsWithDup(int[] nums) {Arrays.sort(nums);
+        List<Integer> output=new ArrayList<>();
+        List<List<Integer>> ans= new ArrayList<>();
+        int i=0;
+        solve(nums,i,output,ans);
+        return ans;
+        
+        
+
+    }
+}
